ec7200a551df5f685e0fe8052f37168c5765ea91
Shinya Okano <tokibito@gmail.com>
2012-03-19 02:19:26 +0900

    アプリケーションのテンプレートを追加
    --HG--
    branch : feedback201203

---
+++ new_application_template/handlers.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+import logging
+import webapp2
+
+import config
+
+from core import wsgi as core_wsgi
+
+
+#class MyAppHandler(webapp2.RequestHandler):
+#    """MyAppハンドラ
+#    """
+#    def get(self):
+#        self.response.headers['Content-Type'] = 'text/plain'
+#        self.response.out.write('OK')
+
+
+application = core_wsgi.WSGIApplication([
+    #(r'.*', MyAppHandler),
+    ], debug=config.DEBUG)
